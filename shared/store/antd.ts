5d9bb48c8121c4dce2b53c7cf3f5c5f3ee20bfdc
milo33 <65894437+milo1150@users.noreply.github.com>
2025-04-07 16:53:22 +0700

    persist dark theme

--- shared/store/antd.ts
+++ shared/store/antd.ts
@@ -1,19 +1,25 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 type AntdStoreState = {
   darkmode: boolean
   setDarkmode: (status: boolean) => void
 }
 
-const useAntdStore = create<AntdStoreState>()((set) => ({
-  darkmode: false,
-  setDarkmode: (status) => {
-    set((state) => {
-      const copyState = { ...state }
-      copyState.darkmode = status
-      return copyState
-    })
-  },
-}))
+const useAntdStore = create<AntdStoreState>()(
+  persist(
+    (set) => ({
+      darkmode: false,
+      setDarkmode: (status) => {
+        set((state) => {
+          const copyState = { ...state }
+          copyState.darkmode = status
+          return copyState
+        })
+      },
+    }),
+    { name: 'antd-store' }
+  )
+)
 
 export { useAntdStore }
